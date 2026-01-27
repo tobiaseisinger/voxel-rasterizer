bec0e406a0795970b4c864eaf8c85c6309bf5ab2
tobiaseisinger <tobias.eisinger@outlook.at>
2026-01-27 18:25:38 +0100

    made the screen render blue

--- src/apphandler.rs
+++ src/apphandler.rs
@@ -1,12 +1,19 @@
+use std::sync::Arc;
+use pixels::{Pixels, SurfaceTexture};
 use winit::application::ApplicationHandler;
-use winit::dpi::LogicalPosition;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, EventLoop};
 use winit::window::{Window, WindowAttributes, WindowId};
 
+
+struct WindowState {
+    window: &'static Window,
+    pixels: Pixels<'static>,
+}
+
 #[derive(Default)]
 pub struct App {
-    window: Option<Window>
+    window_state: Option<WindowState>
 }
 
 impl ApplicationHandler for App {
@@ -16,21 +23,40 @@ impl ApplicationHandler for App {
             .with_inner_size(winit::dpi::LogicalSize::new(640.0, 480.0))
             .with_resizable(false);
 
-        self.window = Some(event_loop.create_window(window_attributes).unwrap());
+        let window = event_loop.create_window(window_attributes).unwrap();
+
+        let window:&'static Window = Box::leak(Box::new(window));
+
+        let surface_texture = SurfaceTexture::new(640, 480, window);
+
+        let pixels = Pixels::new(640, 480, surface_texture).expect("Failed to create pixels");
+
+        self.window_state = Some(WindowState {window, pixels});
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, window_id: WindowId, event: WindowEvent) {
-        match event {
-            WindowEvent::CloseRequested => {
-                println!("Closing window!");
-                event_loop.exit();
-            },
-            WindowEvent::RedrawRequested => {
-                // draw methods
+        if let Some(state) = &mut self.window_state {
+            match event {
+                WindowEvent::CloseRequested => {
+                    println!("Closing window!");
+                    event_loop.exit();
+                },
+                WindowEvent::RedrawRequested => {
+                    let frame = state.pixels.frame_mut();
 
-                self.window.as_ref().unwrap().request_redraw();
-            },
-            _ => (),
+                    for pixel in frame.chunks_exact_mut(4) {
+                        pixel[0] = 0x00;
+                        pixel[1] = 0x00;
+                        pixel[2] = 0xFF;
+                        pixel[3] = 0xFF;
+                    }
+
+                    state.pixels.render().unwrap();
+
+                    state.window.request_redraw();
+                },
+                _ => (),
+            }
         }
     }
 }
