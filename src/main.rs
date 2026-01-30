b8c28b8fdb340f27566c40c091fc47c1841754ed
tobiaseisinger <tobias.eisinger@outlook.at>
2026-01-30 09:49:08 +0100

    added SDL2 because fuck winit and pixels this shit looks as to work with

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-mod apphandler;
-
-use winit::event_loop::{ControlFlow, EventLoop};
-use crate::apphandler::App;
-
 fn main() {
-    let event_loop = EventLoop::new().unwrap();
 
-    event_loop.set_control_flow(ControlFlow::Poll);
-
-    let mut app = App::default();
-    event_loop.run_app(&mut app).expect("PANIC! Can't start the application");
 }
